415b3cc03ad2bb09614277bd92ab015a9168723a
Chow0828 <cristhoferchow9@gmail.com>
2021-11-11 22:52:27 -0300

    first commit

---
+++ licaods/frmprincipal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace licaods
+{
+    public partial class menuStrip1 : Form
+    {
+        public menuStrip1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void usuarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmusuario fu = new frmusuario();
+            fu.ShowDialog();
+        }
+
+        private void clienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmcliente fu = new frmcliente();
+            fu.ShowDialog();
+        }
+
+        private void fornecedorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmfornecedor fu = new frmfornecedor();
+            fu.ShowDialog();
+        }
+
+        private void produtoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmproduto fu = new frmproduto();
+            fu.ShowDialog();
+        }
+    }
+}
